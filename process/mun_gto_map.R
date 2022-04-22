ec0e0821a0d79da60e7d8ecc0a13e68fca3dd3c3
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2022-04-22 17:58:18 -0500

    Se realiza zoom en Guanajuato.
    Implementación del zoom en el estado de Guanajuato.
    Save plot in disk.

--- process/mun_gto_map.R
+++ process/mun_gto_map.R
@@ -15,17 +15,22 @@ library(ggtext)
 suppressMessages(source("source/raw_municipios.R"))
 
 
+nombres_mun <- da_mun_01$Municipio
+
+
 ## Mapa
 # población analfabeta
 map_gto_analfabeta <- da_mun_01 %>% 
   select(region = cla_munic,
          value = analfabeta,
-         Entidad)
+         Municipio)
 
 
 # Mapa
 mxmunicipio_choropleth(map_gto_analfabeta,
-                       legend = "% Población") +
+                       legend = "% Población",
+                       zoom = subset(map_gto_analfabeta,
+                                     Municipio %in% nombres_mun)$region) +
   theme(text = element_text(family = "Optima"),
         plot.title = element_text(size = 18),
         plot.title.position = "plot",
@@ -33,11 +38,12 @@ mxmunicipio_choropleth(map_gto_analfabeta,
         plot.caption = element_markdown(color = "darkgrey",
                                         hjust = 0),
         legend.position = "right") +
-  labs(title = "Porcentaje de Población Analfabeta",
+  labs(title = "Porcentaje de Población Analfabeta en Guanajuato",
        subtitle = "Los números y el gradiente de color indican el porcentaje\nde personas mayores de 15 años que no saben leer.",
        caption = "Fuente: Datos del CONEVAL (2020) <br>
-       Visualización: Juan L. Bretón, PMP | @BretonPmp")
+       Visualización: Juan L. Bretón, PMP | @BretonPmp") #-> mi_02
 
+ggsave(filename = "mi_02", plot = mi_02, path = "figures/", device = "tiff")
 
 
 
