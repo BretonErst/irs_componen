6da6ff8f3b50c241eabbe2c24526b672af8982e4
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2023-03-22 14:50:28 -0600

    Library for map creation updated to sf.

--- process/est_map.R
+++ process/est_map.R
@@ -7,7 +7,7 @@
 
 ## Librerías
 library(tidyverse)
-library(mxmaps)
+library(sf)
 library(ggtext)
 
 
@@ -15,27 +15,47 @@ library(ggtext)
 suppressMessages(source("source/raw_estados.R"))
 
 
+## Preparación del Mapa
+# población educación básica incompleta
+mapa_todas <- da_est_01 %>% 
+  janitor::clean_names() 
+  
+
+# carga del archivo shape
+nal_map <- st_read("source/conjunto_de_datos/nal/00ent.shp")
+
+
+# integración con el archivo de datos
+base_mapa_nacional <- nal_map %>% 
+  left_join(mapa_todas, 
+            by = c("CVEGEO" = "clave"))
+
+
 ## Mapa
-# población analfabete
-map_analfabeta <- da_est_01 %>% 
-  select(region = Clave,
-         value = pob_analfabeta)
-
-
-mxstate_choropleth(map_analfabeta,
-                   legend = "% Población") +
-  theme(text = element_text(family = "Optima"),
-        plot.title = element_text(size = 18),
-        plot.title.position = "plot",
-        plot.caption.position = "plot",
-        plot.caption = element_markdown(color = "darkgrey",
-                                        hjust = 0),
-        legend.position = "right") +
-  labs(title = "Porcentaje de Población Analfabeta",
-       subtitle = "Los números y el gradiente de color indican el porcentaje\nde personas mayores de 15 años que no saben leer.",
-       caption = "Fuente: Datos del CONEVAL (2020) <br>
-       Visualización: Juan L. Bretón, PMP | @BretonPmp")
-
+# educación básica incompleta
+base_mapa_nacional %>% 
+  ggplot(aes(fill = educa_basica_incomp)) +
+    geom_sf(color = "grey60",
+            size = 0.7) +
+    theme(text = element_text(family = "Encode Sans Condensed"),
+          plot.title = element_text(face = "bold", 
+                                    size = 16),
+          panel.background = element_rect(fill = "#FFFFFF"),
+          panel.grid = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.line = element_blank(),
+          plot.caption = element_markdown(color = "darkgrey",
+                                          hjust = 0),
+          plot.title.position = "plot",
+          plot.caption.position = "plot") +
+    labs(title = "Población con educación básica incompleta",
+         subtitle = "Por entidad",
+         caption = "Fuente: CONEVAL 2020<br>
+               Visualización: Juan L. Bretón, PMP | @BretonPmp") +
+    scale_fill_gradient(name = "Porcentaje de\npersonas", 
+                        low = "#FFFFFF",
+                        high = "#7B0303")
 
 
 
