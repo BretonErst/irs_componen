f9e9f59c309730ff0dce165f9f54b1fde9866ca4
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2023-03-22 15:39:52 -0600

    Code for map upadated to feature sf library.

--- process/mun_gto_map.R
+++ process/mun_gto_map.R
@@ -7,7 +7,7 @@
 
 ## Librerías
 library(tidyverse)
-library(mxmaps)
+library(sf)
 library(ggtext)
 
 
@@ -15,35 +15,50 @@ library(ggtext)
 suppressMessages(source("source/raw_municipios.R"))
 
 
-nombres_mun <- da_mun_01$Municipio
+
+## Carga del archivo shape
+gto_map <- st_read("source/conjunto_de_datos/gto/11mun.shp")
 
 
-## Mapa
-# población analfabeta
-map_gto_analfabeta <- da_mun_01 %>% 
-  select(region = cla_munic,
-         value = analfabeta,
-         Municipio)
+# integración con base de datos
+base_gto_mapa <- gto_map %>% 
+  left_join(da_mun_01, by = c("CVEGEO" = "cla_munic"))
 
 
-# Mapa
-mxmunicipio_choropleth(map_gto_analfabeta,
-                       legend = "% Población",
-                       zoom = subset(map_gto_analfabeta,
-                                     Municipio %in% nombres_mun)$region) +
-  theme(text = element_text(family = "Optima"),
-        plot.title = element_text(size = 18),
-        plot.title.position = "plot",
-        plot.caption.position = "plot",
-        plot.caption = element_markdown(color = "darkgrey",
-                                        hjust = 0),
-        legend.position = "right") +
-  labs(title = "Porcentaje de Población Analfabeta en Guanajuato",
-       subtitle = "Los números y el gradiente de color indican el porcentaje\nde personas mayores de 15 años que no saben leer.",
-       caption = "Fuente: Datos del CONEVAL (2020) <br>
-       Visualización: Juan L. Bretón, PMP | @BretonPmp") #-> mi_02
 
-ggsave(filename = "mi_02", plot = mi_02, path = "figures/", device = "tiff")
+## Visualización de mapa
+# con piso de tierra
+
+limite <- round(c(min(base_gto_mapa$piso_tierra),
+                  max(base_gto_mapa$piso_tierra)),
+                digits = 1)
+
+base_gto_mapa %>% 
+  ggplot(aes(fill = piso_tierra)) +
+    geom_sf(color = "grey60",
+            size = 0.7) +
+    theme(text = element_text(family = "Encode Sans Condensed"),
+          plot.title = element_text(face = "bold", 
+                                    size = 16),
+          panel.background = element_rect(fill = "#FFFFFF"),
+          panel.grid = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.line = element_blank(),
+          plot.caption = element_markdown(color = "darkgrey",
+                                          hjust = 0),
+          plot.title.position = "plot",
+          plot.caption.position = "plot") +
+    labs(title = "Población con piso de tierra",
+         subtitle = "Por municipio",
+         caption = "Fuente: CONEVAL Índice de Rezago Social 2020<br>
+                 Visualización: Juan L. Bretón, PMP | @BretonPmp") +
+    scale_fill_gradient(name = "Porcentaje de\npersonas", 
+                        low = "#FFFFFF",
+                        high = "#0465AA",
+                        limits = limite,
+                        breaks = round(seq(limite[1], limite[2], length.out = 4),
+                                       digits = 1))
 
 
 
